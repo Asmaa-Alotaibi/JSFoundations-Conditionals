9221351a8b10cc4d8b5a3f0f8545964a95f3b371
Asmaa-Alotaibi <asmaa-otaibi@yahoo.com>
2020-10-19 17:28:41 +0300

    task 3

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 // Define the numbers and the operation here
+num1=4;
+num2=3;
+op="-"
 
 // Write if-statements here
+if (op=="+")
+console.log(`num1 + num2=  ${num1+num2}  `);
+else if (op=="-")
+    console.log(`num1 - num2=  ${num1-num2}  `);
+    else if (op=="*")
+           console.log(`num1 * num2=  ${num1*num2}  `);
+           else if (op=="/")
+           console.log(`num1 * num2=  ${num1/num2}  `);
